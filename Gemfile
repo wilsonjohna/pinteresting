f352174fdbb785cae4709f9d2f234cb4f8882c6b
John Wilson <wilsonjohna@icloud.com>
2016-11-10 12:12:31 -0600

    Added/Configured the devise gem

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'bootstrap-sass'
 
+gem 'devise', '~> 4.2.0'
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for devise gem
+  # For heroku will need host name
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
