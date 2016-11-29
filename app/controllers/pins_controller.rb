3ee60ab93a42e39af2d2c3ff766676318612b362
John Wilson <wilsonjohna@icloud.com>
2016-11-29 14:46:51 -0600

    attempt to fix heroku attr_accessor

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -65,6 +65,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description, :image)
+      params.require(:pin).permit(:description, :image, :image_file_name)
     end
 end
