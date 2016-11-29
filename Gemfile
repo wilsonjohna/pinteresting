0284540e745d360f37c8991511f28d740492b68e
John Wilson <wilsonjohna@icloud.com>
2016-11-29 15:29:48 -0600

    add s3 for production

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'devise', '~> 4.2.0'
 
 gem 'paperclip', '~> 4.2'
 
+gem 'aws-sdk', '~> 2'
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,3 +88,14 @@ Rails.application.configure do
   # For heroku will need host name
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
+
+#environment variables for amazon s3
+config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+}
