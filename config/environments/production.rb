9f56b6e7315b3194470b3abb7cbfa16eb6588994
John Wilson <wilsonjohna@icloud.com>
2016-11-29 16:41:42 -0600

    try different format for config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,11 +91,10 @@ end
 
 #environment variables for amazon s3
 config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
