4f6a957cb49b4c9e751bc18482e5a17d7c74f9b7
John Wilson <wilsonjohna@icloud.com>
2016-11-29 18:12:35 -0600

    add region var to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,14 +88,15 @@ Rails.application.configure do
   # For heroku will need host name
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-#environment variables for amazon s3
-config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #environment variables for amazon s3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :s3_region => ENV['AWS_REGION']
+    }
   }
-}
 
 end
